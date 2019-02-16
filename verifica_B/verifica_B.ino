c7b38a3d05ba84ac0e4aee8431863b539377705f
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-02-16 09:32:50 +0100

    verifica commentata

--- verifica_B/verifica_B.ino
+++ verifica_B/verifica_B.ino
@@ -3,7 +3,7 @@ int rosso = 13;
 int giallo = 11;
 int blu = 7;
 int verde = 5;
-
+//variabili di tempo
 int tempoRosso = 0;
 int tempoGiallo = 0;
 int tempoBlu = 0;
@@ -12,7 +12,7 @@ int tempoVerde = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+  //output
   pinMode(rosso, OUTPUT);
   pinMode(giallo, OUTPUT);
   pinMode(blu, OUTPUT);
@@ -41,16 +41,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(rosso, HIGH);
-  delay(tempoRosso * 1000);
-  digitalWrite(rosso, LOW);
-  digitalWrite(giallo, HIGH);
-  delay(tempoGiallo * 1000);
-  digitalWrite(giallo, LOW);
-  digitalWrite(blu, HIGH);
-  delay(tempoBlu * 1000);
-  digitalWrite(blu, LOW);
-  digitalWrite(verde, HIGH);
-  delay(tempoVerde * 1000);
-  digitalWrite(verde, LOW);
+  digitalWrite(rosso, HIGH);         //accendo il rosso
+  delay(tempoRosso * 1000);          //aspetto i secondi dati in input per il rosso
+  digitalWrite(rosso, LOW);          //spengo il rosso
+  digitalWrite(giallo, HIGH);        //accendo il giallo
+  delay(tempoGiallo * 1000);         //aspetto i secondi dati in input per il giallo
+  digitalWrite(giallo, LOW);         //spengo il giallo
+  digitalWrite(blu, HIGH);           //accendo il blu
+  delay(tempoBlu * 1000);            //aspetto i secondi dati in input per il blu
+  digitalWrite(blu, LOW);            //spengo il blu
+  digitalWrite(verde, HIGH);         //accendo il verde
+  delay(tempoVerde * 1000);          //aspetto i secondi dati in input per il verde
+  digitalWrite(verde, LOW);          //spengo il verde
 }
